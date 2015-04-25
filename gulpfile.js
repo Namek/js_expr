bc699c0fbe433c6a41df3600ff5fa37ea2fdda21
Kamil Dąbrowski <nameczanin@gmail.com>
2015-04-25 23:27:58 +0200

    Add watch task to gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,11 @@
 var gulp = require('gulp'),
 	gutil = require('gulp-util'),
 	del = require('del'),
+	plumber = require('gulp-plumber'),
 	uglify = require('gulp-uglify'),
 	size = require('gulp-size')
-	peg = require('gulp-peg');
+	peg = require('gulp-peg'),
+	watch = require('gulp-watch');
 
 var Path = {
 	input: 'js.pegjs',
@@ -16,6 +18,7 @@ gulp.task('peg', ['clean'], function() {
 		optimize: 'size'
 	};
 	return gulp.src(Path.input)
+		.pipe(plumber())
 		.pipe(peg(pegOpts).on('error', gutil.log))
 		.pipe(uglify())
 		.pipe(size())
@@ -24,6 +27,12 @@ gulp.task('peg', ['clean'], function() {
 
 gulp.task('clean', function(cb) {
 	del(Path.output, cb);
+});
+
+gulp.task('watch', ['peg'], function() {
+	watch(Path.input, { verbose: true }, function() {
+		gulp.run('peg');
+	});
 });
 
 gulp.task('default', ['peg']);
